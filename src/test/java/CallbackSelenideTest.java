cfc49d7f3d89a8f6500e34c9da63ff45d2be4ba5
dmitriyn <d.nikitin.rf@gmail.com>
2024-03-27 13:56:16 +0500

    add CI

--- src/test/java/CallbackSelenideTest.java
+++ src/test/java/CallbackSelenideTest.java
@@ -23,32 +23,17 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
-/// Исходный тест
-/*
- class CallbackTest {
-    @Test
-    void shouldTest() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-    }
-}*/
 
 class CallbackSelenideTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        ElementsCollection form = $$("[data-test-id]");
-        SelenideElement button = $("button");
+        SelenideElement form = $("form");
 
-        form.get(0).find("[data-test-id=name] input").setValue("Василий");
-        form.get(1).find("[data-test-id=phone] input").setValue("+79270000000");
-        form.get(2).click();
-        button.click();
+        form.find("[data-test-id=name] input").setValue("Василий");
+        form.find("[data-test-id=phone] input").setValue("+79270000000");
+        form.find("[data-test-id='agreement']").click();
+        form.find("button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
